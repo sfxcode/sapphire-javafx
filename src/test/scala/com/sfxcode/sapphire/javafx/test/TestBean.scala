04f74dc50025dcefebb2870d9d70173ca889932f
Thomas Lamers <thomas.lamers@iwelt.de>
2020-12-03 21:17:46 +0100

    formatting

--- src/test/scala/com/sfxcode/sapphire/javafx/test/TestBean.scala
+++ src/test/scala/com/sfxcode/sapphire/javafx/test/TestBean.scala
@@ -1,16 +1,15 @@
 package com.sfxcode.sapphire.javafx.test
 
-import javafx.beans.property.{Property, SimpleStringProperty}
+import javafx.beans.property.{ Property, SimpleStringProperty }
 
 case class Zip(value: Long = 12345)
 
 case class TestBean(
-    name: String = "test",
-    age: Int = 42,
-    zip: Zip = Zip(),
-    description: Option[String] = Some("desc"),
-    observable: Property[_] = new SimpleStringProperty("observable")
-) {
+  name: String = "test",
+  age: Int = 42,
+  zip: Zip = Zip(),
+  description: Option[String] = Some("desc"),
+  observable: Property[_] = new SimpleStringProperty("observable")) {
   def doubleAge(): Int = age * 2
 
   def multiply(first: java.lang.Long, second: java.lang.Long): Long = first * second
