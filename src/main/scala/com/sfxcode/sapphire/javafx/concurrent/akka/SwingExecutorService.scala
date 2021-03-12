a761a3295b1eb2406de8d26eb744a0490fd34078
Thomas Lamers <thomas.lamers@iwelt.de>
2021-03-12 16:06:14 +0100

    delete useless swing

--- src/main/scala/com/sfxcode/sapphire/javafx/concurrent/akka/SwingExecutorService.scala
+++
@@ -1,7 +0,0 @@
-package com.sfxcode.sapphire.javafx.concurrent.akka
-
-import javax.swing.SwingUtilities
-
-object SwingExecutorService extends GUIExecutorService {
-  override def execute(command: Runnable): Unit = SwingUtilities.invokeLater(command)
-}
